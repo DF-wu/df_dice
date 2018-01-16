14cc66db9bffb0855d9c8f2b3097023833cb32c3
df <a2470abc@gmail.com>
2018-01-16 14:34:24 +0800

    file write funtion added

--- main.cpp
+++ main.cpp
@@ -30,31 +30,42 @@ int main()
 
     if (fp == NULL)
     {
-        printf("moteher fucker\n");
+        printf("moteher fucker creat the file first!\n");
         return 0;
     }
 
     int counter = 1;
     dice_st *dice = (dice_st *)calloc(1000, sizeof(*dice));
 
+    //on screen display
     printf("指令如下：\n");
     printf("1.直接輸入骰子點數 用空格隔開 Example: 2 5 6\n");
     printf("2.如果要查看歷史紀錄 請輸入7 7 7\n");
+    
+    //write to buffer of log file
+    fprintf(fp,"指令如下：\n");
+    fprintf(fp,"1.直接輸入骰子點數 用空格隔開 Example: 2 5 6\n");
+    fprintf(fp,"2.如果要查看歷史紀錄 請輸入7 7 7\n");
 
+
+    
     while (scanf("%d%d%d", &dice[counter].a, &dice[counter].b, &dice[counter].c) != EOF)
     {
+        /*
         printf("指令如下：\n");
         printf("1.直接輸入骰子點數 用空格隔開 Example: 2 5 6\n");
         printf("2.如果要查看歷史紀錄 請輸入7 7 7\n");
+        */
 
         if ((dice[counter].a < 7) && (dice[counter].b < 7) && (dice[counter].c < 7))
-        {
+        {   //write into log file
+            fprintf(fp,"第%d個 %d %d %d\n",counter,dice[counter].a,dice[counter].b,dice[counter].c);
             counter++;
         }
 
         else if ((dice[counter].a == 7) && (dice[counter].b == 7) && (dice[counter].c == 7))
         {
-            printf("history\n");
+            printf("歷史紀錄:\n");
             for (i = 0; i < counter; i++)
             {
 
